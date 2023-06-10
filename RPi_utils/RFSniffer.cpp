15de66820932d7d40201d194f178419dcce651c4
circle 2π <124114843+circle2jt@users.noreply.github.com>
2023-06-10 20:24:22 +0700

    fix: Allow pass PIN to args

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
      // This pin is not the first pin on the RPi GPIO header!
      // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
      // for more information.
-     int PIN = 2;
+     int PIN = atoi(argv[1]);
      
      if(wiringPiSetup() == -1) {
        printf("wiringPiSetup failed, exiting...");
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
      }
 
      int pulseLength = 0;
-     if (argv[1] != NULL) pulseLength = atoi(argv[1]);
+     if (argv[2] != NULL) pulseLength = atoi(argv[2]);
 
      mySwitch = RCSwitch();
      if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
